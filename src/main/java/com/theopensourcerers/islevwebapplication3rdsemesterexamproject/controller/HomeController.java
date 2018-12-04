364099de507e4feb639618e4ca19d6f8d2eba1a0
Marco Romeri <marco@romeri.dk>
2018-12-04 12:16:06 +0100

    Log Ind for admin is made, and for member, needs testing

--- src/main/java/com/theopensourcerers/islevwebapplication3rdsemesterexamproject/controller/HomeController.java
+++ src/main/java/com/theopensourcerers/islevwebapplication3rdsemesterexamproject/controller/HomeController.java
@@ -1,12 +1,28 @@
 package com.theopensourcerers.islevwebapplication3rdsemesterexamproject.controller;
 
+import com.theopensourcerers.islevwebapplication3rdsemesterexamproject.base.Session;
+import com.theopensourcerers.islevwebapplication3rdsemesterexamproject.repository.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    SessionRepository sessionRepository;
+
 	@GetMapping("/")
 	public String index(){
 		return "index";
 	}
+
+	@GetMapping("/login")
+    public String loginView() {
+	    //sessionRepository.save(new Session("admin@admin.dk", "admin", "ADMIN"));
+	    //sessionRepository.save(new Session("medlem@medlem.dk", "medlem", "MEDLEM"));
+
+	    return "login";
+    }
+
 }
